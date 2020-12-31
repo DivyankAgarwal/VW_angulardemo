b6ac8a3be6a273385d6df69edb3f2baf1c8b7408
DivyankAgarwal <divyankagarwal7@gmail.com>
2020-12-31 16:51:38 +0530

    3 files modified, 12 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,38 @@ import { Component, VERSION } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = "Hello VWIT";
+  title = "Hello VWIT";
+  name = "Angular";
   a: string = "This is my variable";
   b = 1099;
+  count = 1;
   message: string = "";
+  arr = ["Apple", "Mango", "Banana", "Orange"];
+  loginFlag = true;
 
+  // hello() {
+  //   if (this.b % 2 == 0) {
+  //     this.message = "This is even number";
+  //     console.log(this.message);
+  //   } else {
+  //     this.message = "This is odd number";
+  //     console.log(this.message);
+  //   }
+  //   console.log("hello world");
+  //   return "hello world " + this.a;
+  // }
   hello() {
-    if (this.b % 2 == 0) {
-      this.message = "This is even number";
-      console.log(this.message);
-    } else {
-      this.message = "This is odd number";
-      console.log(this.message);
-    }
+    this.title = "You clicked button";
     console.log("hello world");
-    return "hello world " + this.a;
+    if (this.loginFlag == false) {
+      this.loginFlag = true;
+    } else {
+      this.loginFlag = false;
+    }
+  }
+  bye() {
+    console.log("hi this is from text box");
+    this.name = "hi" + this.count;
+    this.count++;
   }
 }
